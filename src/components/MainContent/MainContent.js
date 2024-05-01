896387929cce04569cb5c871cefb3f3c67837e18
sbennett <susannah-bennett@hotmail.com>
2024-05-01 16:31:01 +0100

    tutorial code

---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+import styles from "./MainContent.module.css";
+
+function MainContent() {
+  return <div className={styles.wrapper}>MainContent</div>;
+}
+
+export default MainContent;
