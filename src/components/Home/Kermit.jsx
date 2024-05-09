d221854fb97337e5fd6d5f659789185e806ebf45
sbennett <susannah-bennett@hotmail.com>
2024-05-09 10:38:41 +0100

    add ribbit button

--- src/components/Home/Kermit.jsx
+++ src/components/Home/Kermit.jsx
@@ -4,18 +4,27 @@ import styles from "./Home.module.css";
 function Kermit({ frogs }) {
   const [currentFrogIndex, setCurrentFrogIndex] = useState(0);
 
+  // Define next frog function with ternary operator
   const nextFrog = () => {
     setCurrentFrogIndex((prevFrog) =>
       prevFrog === frogs.length - 1 ? 0 : prevFrog + 1
     );
   };
 
+  // Define previous frog function with ternary operator
   const previousFrog = () => {
     setCurrentFrogIndex((prevFrog) =>
       prevFrog === 0 ? frogs.length - 1 : prevFrog - 1
     );
   };
 
+  // Define ribbit function
+  function ribbit() {
+    const audio = new Audio("sounds/ribbet.mp3");
+    audio.play();
+  }
+
+  // JSX code for UI of Kermit component
   return (
     <div className={styles.container}>
       <img
@@ -23,11 +32,16 @@ function Kermit({ frogs }) {
         className={styles.frogimage}
         alt={frogs[currentFrogIndex].name}
       />
+      <figcaption className={styles.desc}>
+        {frogs[currentFrogIndex].description}
+      </figcaption>
       <div className={styles.buttoncontainer}>
         <button onClick={previousFrog} className={styles.previous}>
           Previous
         </button>
-        <button className={styles.button}>Ribbet</button>
+        <button onClick={ribbit} className={styles.button}>
+          Ribbit
+        </button>
         <button onClick={nextFrog} className={styles.next}>
           Next
         </button>
