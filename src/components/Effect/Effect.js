0af3c0d38e7b3dec502c55370b80ed988bd1acab
sbennett <susannah-bennett@hotmail.com>
2024-05-13 21:41:33 +0100

    add final tweaks to effect page

--- src/components/Effect/Effect.js
+++ src/components/Effect/Effect.js
@@ -4,7 +4,7 @@ import Clock from "./Clock.jsx";
 function Effect() {
   return (
     <div className={styles.wrapper}>
-      <p className={styles.technique}>Effect Hook</p>
+      <p className={styles.technique}>React Clock using the useEffect Hook</p>
       <Clock />
     </div>
   );
