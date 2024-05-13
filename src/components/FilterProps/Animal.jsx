cb09d1949f07b5d73ebabbd3b8c70e1b9d632681
sbennett <susannah-bennett@hotmail.com>
2024-05-13 20:28:19 +0100

    added alt tags

--- src/components/FilterProps/Animal.jsx
+++ src/components/FilterProps/Animal.jsx
@@ -3,7 +3,11 @@ import styles from "./FilterProps.module.css";
 function Animal({ id, title, description, image }) {
   return (
     <div className={styles.animaldiv}>
-      <img src={`animaljpg/${image}`} className={styles.animalimg} />
+      <img
+        src={`animaljpg/${image}`}
+        className={styles.animalimg}
+        alt={title}
+      />
       <p>{title}</p>
     </div>
   );
